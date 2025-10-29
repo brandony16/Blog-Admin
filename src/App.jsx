6a6bc10c05ff07a7107cd38bc37711660a13dc52
brandony16 <108242812+brandony16@users.noreply.github.com>
2025-10-28 20:03:17 -0400

    Set up project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div>
+        Hello Hello. Last time that I saw your face was recess in second grade
+        and it made me feel young.
+      </div>
+    </>
+  );
+}
+
+export default App;
